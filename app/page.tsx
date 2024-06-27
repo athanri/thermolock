46e31afe9a40534e85953fa1c23c16fd935cd671
Pierce Jordan <piercejord@gmail.com>
2024-06-27 17:31:02 +0100

    PJ - Updated HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -26,21 +26,19 @@ export default function Web() {
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+              Thermolock.ie
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius illo voluptates fuga aspernatur eos voluptatum placeat quis, aliquam quo ab iste ullam, accusamus tenetur commodi adipisci dignissimos eaque optio?
             </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
+            <Button href="#" className="mr-3">
               Get started
             </Button>
             <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+              href="#"
               intent="secondary"
             >
-              Deploy Now
+              Contact Us
             </Button>
           </div>
         </div>
